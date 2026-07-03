6446b95a1a0866cd7e0789712ed9112fe907c787
Arcanuns <andre_bragadioli@hotmail.com>
2026-07-03 20:18:06 -0300

    Update DemoApplicationTests.java

--- Downloads/demo/demo/src/test/java/com/example/demo/DemoApplicationTests.java
+++ Downloads/demo/demo/src/test/java/com/example/demo/DemoApplicationTests.java
@@ -1,25 +1,12 @@
 package com.example.demo;
 
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootApplication
-@RestController
-public class DemoApplication {
+@SpringBootTest
+class DemoApplicationTests {
 
-	public static void main(String[] args) {
-		SpringApplication.run(DemoApplication.class, args);
-	}
-
-	@GetMapping("/")
-	public String home() {
-		return "Spring Boot rodando!";
-	}
-
-	@GetMapping("/hello")
-	public String hello() {
-		return "Hello AWS!";
-	}
+    @Test
+    void contextLoads() {
+    }
 }
